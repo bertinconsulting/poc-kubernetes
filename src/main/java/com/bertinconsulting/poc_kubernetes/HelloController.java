77e377199b9aeef8bcd7e5a2a9f3e3d222addded
tim <tbertinconsulting@gmail.com>
2026-09-16 13:28:11 +0200

    Chapitre 4 done

--- src/main/java/com/bertinconsulting/poc_kubernetes/HelloController.java
+++ src/main/java/com/bertinconsulting/poc_kubernetes/HelloController.java
@@ -1,15 +1,18 @@
 package com.bertinconsulting.poc_kubernetes;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello")
 public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
         throw new RuntimeException("Version cassée !");
+    }
+
+    @GetMapping("/version")
+    public String version() {
+        return "Version 4.0";
     }
 }
